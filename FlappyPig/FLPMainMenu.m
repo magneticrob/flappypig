c8af52c08fd9f66668088fcdb0facb76bbb73805
Robert Baker <robertbaker0@gmail.com>
2014-02-09 14:03:56 +0000

    Added effect on main header

--- FlappyPig/FLPMainMenu.m
+++ FlappyPig/FLPMainMenu.m
@@ -30,9 +30,20 @@
     FLPShadowLabelNode *mainHeader = [FLPShadowLabelNode shadowLabelWithSKLabel:[SKLabelNode labelNodeWithFontNamed:@"MineCrafter-2.0"]];
     mainHeader.text = @"FLAPPY PIG";
     mainHeader.fontSize = 72;
-    mainHeader.position = CGPointMake(self.size.width * 0.5, self.size.height * 0.7);
+    mainHeader.position = CGPointMake(self.size.width * 0.5, self.size.height);
+    
+    mainHeader.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:[@"FLAPPY PIG" sizeWithAttributes:@{ NSFontAttributeName : [UIFont fontWithName:@"MineCrafter-2.0" size:40.0] }]];
+    mainHeader.zRotation = mainHeader.zRotation + 0.1;
+    mainHeader.physicsBody.restitution = 0.4;
     
     [self addChild:mainHeader];
+    
+    // create a transparent barrier for the main header label to smash into
+    SKSpriteNode *transparentBarrier = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(self.size.width, 1)];
+    transparentBarrier.position = CGPointMake(self.size.width * 0.5, self.size.height * 0.7);
+    transparentBarrier.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:transparentBarrier.size];
+    transparentBarrier.physicsBody.dynamic = NO;
+    [self addChild:transparentBarrier];
 }
 
 - (void)addButtons
